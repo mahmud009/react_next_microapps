1ea1e7d0d6a5abf97bfd136cd2a45846fdf595a7
mahmud009 <mahmud.est@gmail.com>
2023-04-06 05:34:01 +0600

    animated sidebar

--- features/animatedPage/ListItem.tsx
+++ features/animatedPage/ListItem.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Box, Text } from "@/reusables/components";
 import { useTheme } from "@/reusables/hooks";
+import { useSpring, animated } from "@react-spring/web";
 
 interface Props {
   icon: React.ReactNode;
@@ -9,18 +10,25 @@ interface Props {
 
 export function ListItem({ icon, text }: Props) {
   const { colors } = useTheme();
+
   return (
     <Box
       display="grid"
       gridTemplateColumns={`32px 1fr`}
       gap="16px"
       alignItems="center"
+      opacity={0.7}
     >
-      <Box color={"#ffffff"} fontSize={"32px"} height={"32px"} width="32px">
+      <Box color={"#ffffff"} fontSize={"24px"} height={"32px"} width="32px">
         {icon}
       </Box>
       <Box>
-        <Text color="#ffffff" fontSize={"18px"} letterSpacing={"0.05em"}>
+        <Text
+          color="#ffffff"
+          fontSize={"18px"}
+          letterSpacing={"0.05em"}
+          fontWeight={400}
+        >
           {text}
         </Text>
       </Box>
--- features/animatedPage/Sidebar.tsx
+++ features/animatedPage/Sidebar.tsx
@@ -1,35 +1,52 @@
 import { Box, List } from "@/reusables/components";
 import { useTheme } from "@/reusables/hooks";
 import { ListItem } from "./ListItem";
-import { BiHome } from "react-icons/bi";
-import { MdOutlineHome } from "react-icons/md";
+import {
+  FiHome,
+  FiCoffee,
+  FiFile,
+  FiGrid,
+  FiMessageCircle,
+} from "react-icons/fi";
 import { v4 as uuid } from "uuid";
+import { animated, useSprings } from "@react-spring/web";
 
 let menuItems = [
   {
-    icon: <MdOutlineHome />,
+    icon: <FiHome />,
     text: "Home",
   },
   {
-    icon: <MdOutlineHome />,
+    icon: <FiCoffee />,
     text: "About",
   },
   {
-    icon: <MdOutlineHome />,
+    icon: <FiFile />,
     text: "Resume",
   },
   {
-    icon: <MdOutlineHome />,
+    icon: <FiGrid />,
     text: "Portfolio",
   },
   {
-    icon: <MdOutlineHome />,
+    icon: <FiMessageCircle />,
     text: "Contact",
   },
 ];
 
 export function SideBar() {
   const theme = useTheme();
+
+  const springs = useSprings(
+    menuItems.length,
+    menuItems.map((item, index) => ({
+      from: { y: 30, opacity: 0 },
+      to: { y: 0, opacity: 1 },
+      delay: index * 150, // set a delay based on the index
+      config: { duration: 600 },
+    }))
+  );
+
   return (
     <Box
       width="350px"
@@ -39,10 +56,18 @@ export function SideBar() {
       justifyContent="center"
       alignItems="center"
     >
-      <List backgroundColor={theme.colors.primary}>
-        {menuItems.map((itm) => (
-          <ListItem key={uuid()} icon={itm.icon} text={itm.text} />
-        ))}
+      <List display="grid" gridTemplateColumns={"1fr"} gap={"16px"}>
+        {springs.map((spring, idx) => {
+          let itm = menuItems[idx];
+
+          return (
+            <Box overflow="hidden" key={uuid()}>
+              <animated.div style={spring}>
+                <ListItem icon={itm.icon} text={itm.text} />
+              </animated.div>
+            </Box>
+          );
+        })}
       </List>
     </Box>
   );
